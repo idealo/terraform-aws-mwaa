285ee3e93d0e31fd433f204e8bfbd123653f997f
Martin Gerlach <martin.gerlach@idealo.de>
2022-03-09 18:31:08 +0100

    fix(INA-1987): address Trusted Advisor recommendation to not specify broad KMS permission in inline policy
    Try to use a managed policy with role-policy-attachment instead.
    
    Cannot follow the instructions here https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-standards-fsbp-controls.html#kms-2-remediation as KMS policy statement uses not_resources to grant KMS permissions for any KMS keys EXCEPT account specific keys. We do not know which specific keys to narrow to.

--- iam.tf
+++ iam.tf
@@ -4,10 +4,15 @@ resource "aws_iam_role" "this" {
   tags               = var.tags
 }
 
-resource "aws_iam_role_policy" "this" {
+resource "aws_iam_policy" "this" {
   name   = "mwaa-${var.environment_name}-execution-policy"
   policy = data.aws_iam_policy_document.this.json
-  role   = aws_iam_role.this.id
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
 }
 
 data "aws_iam_policy_document" "assume" {
