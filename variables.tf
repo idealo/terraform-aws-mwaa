6f801ad5ecf8f021bfb048bf8778a8093a74e837
Martin Gerlach <martin.gerlach@idealo.de>
2022-03-10 16:12:24 +0100

    feat: Allow setting log levels for logging_configuration (#58)

--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 # general information
 variable "region" {
-  type = string
+  type        = string
   description = "AWS Region where the environment and its resources will be created"
 }
 variable "account_id" {
-  type = string
+  type        = string
   description = "Account ID of the account in which MWAA will be started"
 }
 variable "environment_name" {
-  type = string
+  type        = string
   description = "Name of the MWAA environment"
 }
 variable "airflow_version" {
   description = "Airflow version to be used"
-  type = string
-  default = "2.0.2"
+  type        = string
+  default     = "2.0.2"
 }
 
 # s3 configuration
 variable "source_bucket_arn" {
-  type = string
+  type        = string
   description = "ARN of the bucket in which DAGs, Plugin and Requirements are put"
 }
 variable "dag_s3_path" {
   description = "Relative path of the dags folder within the source bucket"
-  type = string
-  default = "dags/"
+  type        = string
+  default     = "dags/"
 }
 variable "plugins_s3_path" {
-  type = string
+  type        = string
   description = "relative path of the plugins.zip within the source bucket"
-  default = null
+  default     = null
 }
 variable "plugins_s3_object_version" {
   default = null
 }
 variable "requirements_s3_path" {
-  type = string
+  type        = string
   description = "relative path of the requirements.txt (incl. filename) within the source bucket"
-  default = null
+  default     = null
 }
 variable "requirements_s3_object_version" {
   default = null
@@ -47,8 +47,8 @@ variable "requirements_s3_object_version" {
 # airflow.cfg values
 variable "airflow_configuration_options" {
   description = "additional configuration to overwrite airflows standard config"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 # networking
@@ -61,32 +61,32 @@ variable "internet_gateway_id" {
 
 variable "create_networking_config" {
   description = "true if networking resources (subnets, eip, NAT gateway and route table) should be created."
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for the public subnets MWAA uses. Must be at least 2 if create_networking_config=true"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for the private subnets MWAA uses. Must be at least 2 if create_networking_config=true"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "private_subnet_ids" {
   description = "Subnet Ids of the existing private subnets that should be used if create_networking_config=false"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 # iam
 variable "additional_execution_role_policy_document_json" {
   description = "Additional permissions to attach to the base mwaa execution role"
-  type = string
-  default = "{}"
+  type        = string
+  default     = "{}"
 }
 
 variable "max_workers" {
@@ -103,8 +103,8 @@ variable "environment_class" {
 
 variable "webserver_access_mode" {
   description = "Default: PRIVATE_ONLY"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "kms_key" {
@@ -114,6 +114,61 @@ variable "kms_key" {
 }
 
 variable "tags" {
-  type = map(string)
+  type    = map(string)
   default = {}
+}
+
+variable "dag_processing_logs_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "dag_processing_logs_level" {
+  type        = string
+  description = "One of: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+  default     = "WARNING"
+}
+
+variable "scheduler_logs_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "scheduler_logs_level" {
+  type        = string
+  description = "One of: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+  default     = "WARNING"
+}
+
+variable "task_logs_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "task_logs_level" {
+  type        = string
+  description = "One of: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+  default     = "INFO"
+}
+
+variable "webserver_logs_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "webserver_logs_level" {
+  type        = string
+  description = "One of: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+  default     = "WARNING"
+}
+
+variable "worker_logs_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "worker_logs_level" {
+  type        = string
+  description = "One of: DEBUG, INFO, WARNING, ERROR, CRITICAL"
+  default     = "WARNING"
 }
