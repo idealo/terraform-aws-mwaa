ecc282f2f7de77567ed353e4817aa3e53864c970
ynaim94 <youssef.naim@harrys.com>
2022-05-16 15:54:33 -0400

    Use splat expression

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "mwaa_arn" {
 }
 
 output "mwaa_nat_gateway_public_ips" {
-  value = var.create_networking_config ? [ for nat_gateway in aws_nat_gateway.this: nat_gateway.public_ip ]: []
+  value = aws_nat_gateway.this[*].public_ip
   description = "List of the ips of the nat gateways created by this module."
 }
