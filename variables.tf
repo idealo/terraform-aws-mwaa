f14e566cbfc05eaf5aacb9ac620de517eaeacfd3
Jakob Diebold <jakob.diebold@idealo.de>
2022-11-21 16:17:04 +0100

    feat: Remove obsolete internet gateway if networking provided (#105)
    * chore(deps): bump terraform-linters/setup-tflint from 2 to 3
    
    Bumps [terraform-linters/setup-tflint](https://github.com/terraform-linters/setup-tflint) from 2 to 3.
    - [Release notes](https://github.com/terraform-linters/setup-tflint/releases)
    - [Commits](https://github.com/terraform-linters/setup-tflint/compare/v2...v3)
    
    ---
    updated-dependencies:
    - dependency-name: terraform-linters/setup-tflint
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore(deps): bump hashicorp/aws from 4.39.0 to 4.40.0
    
    Bumps [hashicorp/aws](https://github.com/hashicorp/terraform-provider-aws) from 4.39.0 to 4.40.0.
    - [Release notes](https://github.com/hashicorp/terraform-provider-aws/releases)
    - [Changelog](https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/hashicorp/terraform-provider-aws/compare/v4.39.0...v4.40.0)
    
    ---
    updated-dependencies:
    - dependency-name: hashicorp/aws
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * terraform-docs: automated action
    
    * feat: make internet_gateway_id optional and create internet_gateway_id if not set
    
    * adapt for review comments
    
    * terraform-docs: automated action
    
    * adapt var description
    
    * terraform-docs: automated action
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Martin Gerlach <martin.gerlach@idealo.de>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -59,8 +59,9 @@ variable "vpc_id" {
   type        = string
 }
 variable "internet_gateway_id" {
-  description = "ID of the internet gateway to the VPC"
+  description = "ID of the internet gateway to the VPC, if not set and create_networking_config = true an internet gateway will be created"
   type        = string
+  default     = null
 }
 variable "create_networking_config" {
   description = "true if networking resources (subnets, eip, NAT gateway and route table) should be created."
