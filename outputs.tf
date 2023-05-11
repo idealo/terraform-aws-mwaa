c4dc148e6d062f2b39a14853d5fba579cdba1e17
H.Ordoñez <hector_ogun_9@hotmail.com>
2023-05-11 21:20:21 +0200

    feat: Add ARN created roles as output (#123)
    * Add ARN created roles as output
    
    * terraform-docs: automated action
    
    * Rollback Readme.md
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: HectorArchilogic <hector@archilogic.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "mwaa_arn" {
   description = "The arn of the created MWAA environment."
 }
 
+output "mwaa_execution_role_arn" {
+  value = aws_mwaa_environment.this.execution_role_arn
+  description = "The IAM Role arn for MWAA Execution Role."
+}
+
+output "mwaa_service_role_arn" {
+  value = aws_mwaa_environment.this.execution_role_arn
+  description = "The Service Role arn for MWAA environment."
+}
+
 output "mwaa_nat_gateway_public_ips" {
   value       = aws_nat_gateway.this[*].public_ip
   description = "List of the ips of the nat gateways created by this module."
