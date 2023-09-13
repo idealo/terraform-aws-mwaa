687bb9ad8e5771df44bc59417bbaab2547284b49
ugmuka <55133701+ugmuka@users.noreply.github.com>
2023-09-13 15:38:57 +0900

    fix: get availability zone from data source (#137)
    * fix: get availability zone from data source
    
    * terraform-docs: automated action
    
    * refactor: fix data source quoting
    
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    
    * refactor: fix data source quoting
    
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    
    * fix: sort az name
    
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    
    * fix: use local variable
    
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    
    * fix: use local variables
    
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ugmuka <55133701+ugmuka@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Gerlach <gerlach.martin@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,20 @@
+data "aws_availability_zones" "available" {
+  filter {
+    name = "group-name"
+    values = [var.region]
+  }
+}
+
+locals {
+  az_names = sort(data.aws_availability_zones.available.names)
+}
+
 resource "aws_subnet" "public" {
   count                   = var.create_networking_config ? length(var.public_subnet_cidrs) : 0
   cidr_block              = var.public_subnet_cidrs[count.index]
   vpc_id                  = var.vpc_id
   map_public_ip_on_launch = true
-  availability_zone       = count.index % 2 == 0 ? "${var.region}a" : "${var.region}b"
+  availability_zone       = count.index % 2 == 0 ? local.az_names[0] : local.az_names[1]
   tags                    = merge({
     Name = "mwaa-${var.environment_name}-public-subnet-${count.index}"
   }, var.tags)
@@ -14,7 +25,7 @@ resource "aws_subnet" "private" {
   cidr_block              = var.private_subnet_cidrs[count.index]
   vpc_id                  = var.vpc_id
   map_public_ip_on_launch = false
-  availability_zone       = count.index % 2 == 0 ? "${var.region}a" : "${var.region}b"
+  availability_zone       = count.index % 2 == 0 ? local.az_names[0] : local.az_names[1]
   tags                    = merge({
     Name = "mwaa-${var.environment_name}-private-subnet-${count.index}"
   }, var.tags)
