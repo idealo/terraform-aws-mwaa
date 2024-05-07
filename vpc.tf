cc611e902551f258a4c6764febfce97fb0208bbd
Jakob Diebold <jakob.diebold@idealo.de>
2024-05-07 16:43:13 +0200

    remove old inline egress rule

--- vpc.tf
+++ vpc.tf
@@ -98,14 +98,6 @@ resource "aws_security_group" "this" {
   tags   = merge({
     Name = "mwaa-${var.environment_name}-no-ingress-sg"
   }, var.tags  )
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
 }
 
 resource "aws_security_group_rule" "ingress_from_self" {
